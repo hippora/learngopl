72a3e7d96b87bfdf8ae0c20c581ca0e0220f06f0
Hippo <Hippo@Hippo-MBA.lan>
2019-06-12 18:05:09 +0800

    update

--- ch4/work4.12/main.go
+++ ch4/work4.12/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -22,22 +22,33 @@ type xkcd struct {
 }
 
 func main() {
-	var result xkcd
-	resp, err := http.Get("https://xkcd.com/571/info.0.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Fatal(err)
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		log.Fatal(err)
-	}
-	resp.Body.Close()
-
-	fmt.Println(result)
+	downloadAll()
 }
 
-func dlall() {
-
+func downloadAll() {
+	var result xkcd
+	for i := 1; i < 10; i++ {
+		url := fmt.Sprintf("https://xkcd.com/%d/info.0.json", i)
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("http get error:%v\n", err)
+			resp.Body.Close()
+			continue
+		}
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("status %d,error:%v\n", resp.StatusCode, err)
+			resp.Body.Close()
+			continue
+		}
+		buf, err := ioutil.ReadAll(resp.Body)
+		if err := json.Unmarshal(buf, &result); err != nil {
+			fmt.Printf("json decode error:%v\n", err)
+			resp.Body.Close()
+			continue
+		}
+		filename := fmt.Sprintf("ch4/work4.12/%d.json", i)
+		ioutil.WriteFile(filename, buf, 0644)
+		resp.Body.Close()
+	}
 }
